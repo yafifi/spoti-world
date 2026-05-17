aee16bbabbd542b32d877868c7ac2bdadeb5742c
Yasmin Afifi <yasmin.h.afifi@gmail.com>
2026-05-16 23:24:57 -0500

    refactored code to include Vite and button callback space

--- main.js
+++
@@ -1,2 +0,0 @@
-console.log("JS running successfully");
-
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './style.css'
+
+console.log("JS running successfully");
+
+const loginButton = document.getElementById("login-button")
+
+loginButton.addEventListener("click", () => {
+    alert("Add Spotify Login Callback Here");
+});
